2604d89137ca0bb3b487751bafa2dbb04a9d2c74
xiaowang <xiaowang@qq.com>
2023-07-04 18:52:16 +0800

    第一次提交vmall_banner的demo

---
+++ js/banner_data.js
@@ -0,0 +1,17 @@
+const bannerData = [
+  // './img/huawei_01.webp',
+  // './img/huawei_02.webp',
+  // './img/huawei_03.webp',
+  // './img/huawei_04.webp',
+  // './img/huawei_05.webp',
+  // './img/huawei_06.webp',
+  // './img/huawei_07.webp',
+  // './img/huawei_08.webp'
+  'https://res.vmallres.com/cmscdn/CN/2023-06/215c3d1ece06474d8bb7b33ff111decf.jpg',
+  'https://res.vmallres.com/cmscdn/CN/2023-06/0e9fee22553d42ef9353b1108ff8758f.jpg',
+  'https://res.vmallres.com/cmscdn/CN/2023-06/8f4794ebf4994c0088e4c3734af5dc27.jpg',
+  'https://res.vmallres.com/cmscdn/CN/2023-07/a53ea4f713324a83b1dba6cb03b14b10.jpg',
+  'https://res.vmallres.com/cmscdn/CN/2023-06/77326d1656f34ba8a54c31834906e9f4.jpg',
+  'https://res.vmallres.com/cmscdn/CN/2023-07/d4cf87fa7f0f4ec8a6466a8974afbabb.jpg',
+  'https://res.vmallres.com/cmscdn/CN/2023-06/9a44bac7bab2498d889ec822d3eae50f.jpg'
+]
